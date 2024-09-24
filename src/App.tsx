a9b7b44f085ce0bdd68d38fd927b09298336e923
Ayrton Lima <ayrtonlima1997@gmail.com>
2024-09-24 16:41:18 -0300

    Formulário API front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>oi!</h1>
+      <div className="Container">
+        <h1>Formulário API</h1>
+        <input
+          style={{
+            width: 150,
+            marginBottom: 10
+          }}
+          placeholder="E-mail"
+        />
+        <input
+          style={{
+            width: 150,
+            marginBottom: 10
+          }}
+          type="password"
+          placeholder="Senha"
+        />
+        <button
+          style={{
+            padding: 5,
+            boxShadow: "1px 1px 1px black"
+          }}>
+          Entrar
+        </button>
+      </div>
     </div>
   );
 }
