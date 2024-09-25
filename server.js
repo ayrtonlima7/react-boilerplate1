ec01a81cff3b3413f0d19192122251eb56f7acc7
Ayrton <ayrtonlima1997@gmail.com>
2024-09-25 17:46:56 -0300

    CRUD

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
+const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Rota para registrar um novo usuário
 app.post('/register', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        // Criar um novo usuário no banco de dados
-        const user = await prisma.user.create({
-            data: {
-                email,
-                password, // Você vai adicionar o hash da senha mais tarde
-            },
-        });
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(400).json({ error: 'Erro ao registrar usuário.' });
-    }
+    const {email, password} = req.body;
+    const user = await prisma.userModel.create({data: {email, password}})
+    res.json(user);
 });
+
+app.get('/register', async (req, res) => {
+    const users = await prisma.userModel.findMany()
+    res.json(users);
+});
+
+app.get('/register/:id', async (req, res) => {
+    const userId = parseInt(req.params.id)
+    const user = await prisma.userModel.findFirst({where: {id: userId}})
+    res.json(user)
+})
+
+app.put('/register/:id', async (req, res) => {
+    const userId = parseInt(req.params.id)
+    const {email, password} = req.body
+    const updatedUser = await prisma.userModel.update({where: { id: userId}, data: {email, password}})
+    res.json(updatedUser)
+})
+
+app.delete('/register/:id', async (req, res) => {
+    const deleteId = parseInt(req.params.id)
+    const user = await prisma.userModel.delete({where: {id: deleteId}})
+    res.json(user)
+})
 
 app.listen(5000, () => {
     console.log('Servidor rodando na porta 5000');
